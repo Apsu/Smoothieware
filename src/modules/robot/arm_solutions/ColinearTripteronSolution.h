c3113762766bb513b3defe3609099423541d8a63
Apsu <apsu@propter.net>
2016-11-29 00:26:23 -0600

    Remove unneeded header include

--- src/modules/robot/arm_solutions/ColinearTripteronSolution.h
+++ src/modules/robot/arm_solutions/ColinearTripteronSolution.h
@@ -1,10 +1,8 @@
 #pragma once
 
 #include "libs/Module.h"
-#include "libs/Kernel.h"
 #include "BaseSolution.h"
 #include "libs/nuts_bolts.h"
-
 #include "libs/Config.h"
 
 class ColinearTripteronSolution : public BaseSolution {
